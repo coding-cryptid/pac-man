6261abf3d5a7978aa3e7028409ff3877c9db2cc7
giannag27 <giacosey28@gmail.com>
2026-02-17 11:39:28 -0500

    Added image variables

--- pacman.js
+++ pacman.js
@@ -1,4 +1,4 @@
-//Board 
+//board 
 let gameBoard;
 const rowCount = 21;
 const colCount = 19;
@@ -6,6 +6,17 @@ const tileSize = 32;
 const gameBoardWidth = colCount * tileSize;
 const gameBoardHeight = rowCount * tileSize;
 let context;
+
+//images
+let blueGhostImage;
+let redGhostImage;
+let pinkGhostImage;
+let orangeGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
 
 window.onload = function() {
     gameBoard = this.document.getElementById("gameBoard");
