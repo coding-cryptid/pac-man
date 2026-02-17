cd0c84d620b9ba97249f5f6399c6bac4fa92b56b
giannag27 <giacosey28@gmail.com>
2026-02-17 11:41:44 -0500

    Added load image function

--- pacman.js
+++ pacman.js
@@ -24,3 +24,32 @@ window.onload = function() {
     gameBoard.width = gameBoardWidth;
     context = gameBoard.getContext("2d");
 }
+
+function loadImages() {
+    wallImage = new Image();
+    wallImage.src = "./wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "./blueGhost.png";
+
+    redGhostImage = new Image();
+    redGhostImage.src = "./redGhost.png";
+
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = "./pinkGhost.png";
+
+    orangeGhostImage = new Image();
+    orangeGhostImage.src = "./orangeGhost.png";
+
+    pacmanUpImage = new Image();
+    pacmanUpImage.src = "./pacmanUp.png";
+
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "./pacmanDown.png";
+
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "./pacmanLeft.png";
+
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "./pacmanRight.png";
+}
